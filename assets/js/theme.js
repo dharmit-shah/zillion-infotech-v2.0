796080191300e50fa16b7e0388751e57f1d9d030
dharmit-shah <dharmitshah1996@gmail.com>
2025-05-23 02:07:47 +0530

    UI Changes and Animation

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -46,6 +46,7 @@ function getIp(callback) {
 }
 const phoneInputField = document.querySelector("#phone");
 const phoneInput = window.intlTelInput(phoneInputField, { utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js", });
+
 function process(event) {
     event.preventDefault();
     const phoneNumber = phoneInput.getNumber();
